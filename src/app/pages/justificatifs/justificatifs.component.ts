c064555f6580a30cad7ef41e801fe91b5315984a
Quentin <rosarquentin@gmail.com>
2019-07-17 17:23:52 +0200

    contenu page justificatifs terminé

---
+++ src/app/pages/justificatifs/justificatifs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-justificatifs',
+  templateUrl: './justificatifs.component.html',
+  styleUrls: ['./justificatifs.component.scss']
+})
+export class JustificatifsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
