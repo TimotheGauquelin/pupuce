3c78e74eefb83aa8cd463fd3bb08e3b2fc3c80d2
Tim <timothegauquelin@gmail.com>
2019-09-24 11:39:52 +0200

    :building_construction: "marche aux puces" router update

--- src/app/pages/marcheAuxPuces/marcheAuxPuces.component.ts
+++ src/app/pages/marcheAuxPuces/marcheAuxPuces.component.ts
@@ -1,7 +1,7 @@
+import {Router, ActivatedRoute, Params } from '@angular/router';
 import {Component} from '@angular/core';
 import {MarchesAuxPucesService} from '../../shared/services/marches-aux-puces.service';
 import {MarcheAuxPuces} from '../../shared/models/marche-aux-puces.model';
-
 @Component({
     selector: 'app-marche-aux-puces',
     templateUrl: './marcheAuxPuces.component.html',
@@ -11,10 +11,18 @@ export class MarcheAuxPucesComponent {
 
     public marchesAuxPuces: MarcheAuxPuces[] = [];
 
-    constructor(private marchesAuxPucesService: MarchesAuxPucesService) {
+
+    constructor(private router: Router, route: ActivatedRoute,
+                private marchesAuxPucesService: MarchesAuxPucesService) {
         marchesAuxPucesService.getAllNextMarchesAuxPuces().subscribe((marchesAuxPuces: MarcheAuxPuces[]) => {
             this.marchesAuxPuces = marchesAuxPuces;
         });
+    }
+
+    goToInformations(id: number): void {
+        this.router.navigateByUrl(
+            `/marche-aux-puces/${id}/informations`
+        );
     }
 
 }
