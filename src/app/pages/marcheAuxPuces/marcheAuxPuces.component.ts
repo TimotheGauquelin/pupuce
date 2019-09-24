d6168d84f33838dc1ba61165720c98007c5e54b2
Tim <timothegauquelin@gmail.com>
2019-09-24 10:46:01 +0200

    :sparkles: display dynamic "marche aux puces"

--- src/app/pages/marcheAuxPuces/marcheAuxPuces.component.ts
+++ src/app/pages/marcheAuxPuces/marcheAuxPuces.component.ts
@@ -1,16 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {MarchesAuxPucesService} from '../../shared/services/marches-aux-puces.service';
+import {MarcheAuxPuces} from '../../shared/models/marche-aux-puces.model';
 
 @Component({
     selector: 'app-marche-aux-puces',
     templateUrl: './marcheAuxPuces.component.html',
     styleUrls: ['./marcheAuxPuces.component.scss']
 })
-export class MarcheAuxPucesComponent implements OnInit {
+export class MarcheAuxPucesComponent {
 
-    constructor() {
-    }
+    public marchesAuxPuces: MarcheAuxPuces[] = [];
 
-    ngOnInit() {
+    constructor(private marchesAuxPucesService: MarchesAuxPucesService) {
+        marchesAuxPucesService.getAllNextMarchesAuxPuces().subscribe((marchesAuxPuces: MarcheAuxPuces[]) => {
+            this.marchesAuxPuces = marchesAuxPuces;
+        });
     }
 
 }
