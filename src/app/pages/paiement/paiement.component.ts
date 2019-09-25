1a5bf38466a47e3349e82e20dcedda2377f32b1d
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2019-09-25 11:32:03 +0200

    :sparkles: improve paiement page

--- src/app/pages/paiement/paiement.component.ts
+++ src/app/pages/paiement/paiement.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {ReservationsService} from '../../shared/services/reservations.service';
 
 @Component({
     selector: 'app-paiement',
@@ -7,4 +9,33 @@ import {Component, Input, OnInit, Output} from '@angular/core';
 })
 export class PaiementComponent {
 
+    private codeReservation: number;
+    private idMarche: number;
+    public sommeAPayer: number;
+
+    constructor(route: ActivatedRoute,
+                private router: Router,
+                private reservationService: ReservationsService) {
+        route.params.subscribe((params: Params) => {
+            this.codeReservation = params.code;
+            this.idMarche = params.id;
+            this.calculSommeAPayer(); // Apelle la fonction au chargement de la page
+        });
+    }
+
+    private redirectionSynthese() {
+        this.router.navigate([`marches-aux-puces/${this.idMarche}/reservations/${this.codeReservation}/synthese`]);
+    }
+
+    private calculSommeAPayer() {
+        this.reservationService.getReservation(this.idMarche, this.codeReservation).subscribe(reservation => {
+            this.sommeAPayer = reservation.montant;
+        });
+    }
+
+    public submitBordereau(formulaire: { modePaiment: string, numeroBorderau: string }) {
+        this.reservationService.setBordereau(this.idMarche, this.codeReservation, formulaire.numeroBorderau).subscribe(() => {
+            this.redirectionSynthese();
+        });
+    }
 }
